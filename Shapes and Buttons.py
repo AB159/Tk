3b6e314cd1a1525779bc651ac04cd2535d0c1e96
Alex <37304058+AB159@users.noreply.github.com>
2018-05-05 14:45:30 -0400

    Keypress

--- Shapes and Buttons.py
+++ Shapes and Buttons.py
@@ -1,26 +1,26 @@
 #Tk Shapes and Buttons
 from tkinter import *
 root = Tk()
-canvas = Canvas(root, width=600, height=600)
+root.title("Shapes and Buttons")
+canvas = Canvas(root, width=400, height=400)
 canvas.pack()
+canvas.create_polygon(10,10, 10,60, 50,35)
 def drawCircle():
-    canvas.create_oval(100,100, 175,175, fill ="blue", outline = "red")
+    canvas.create_oval(170,170, 200,200, fill ="blue", outline = "red")
 def drawRec():
-    canvas.create_rectangle(10,10, 50,50, fill = "blue", outline = "red")
+    canvas.create_rectangle(300,300, 250,200, fill = "blue", outline = "red")
 def drawSquare():
-    canvas.create_rectangle(55,55, 110,110, fill = "blue", outline = "red")
+    canvas.create_rectangle(150,150, 100,100, fill = "blue", outline = "red")
 def moveTri(event):
     if event.keysm == "w":
         canvas.move(1, 0, -3)
     if event.keysm == "s":
         canvas.move(1, 0, 3)
     if event.keysm == "a":
-        canvas.move(1, 0, -3)
-    if event.keysm == "s":
-        canvas.move(1, 0, -3)
-
-
-    
+        canvas.move(1, -3, 0)
+    if event.keysm == "d":
+        canvas.move(1, 3, 0)
+canvas.bind_all('KeyPress>', moveTri)
     
 
 
